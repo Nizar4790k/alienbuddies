a81103bc7a9763dffbd985b1ebb41873cc60f081
Nizar <nizar4790k@gmail.com>
2020-10-27 10:09:04 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './Hello';
+import 'tachyons';
+
+import './index.css';
+
+ReactDOM.render(
+  null,
+  document.getElementById('root')
+);
